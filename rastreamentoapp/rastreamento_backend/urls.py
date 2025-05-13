19b30dba07cbf17ae28d3c97e26679b4ec96894e
Jhones Soares Neves <jhones.soares.neves@gmail.com>
2025-05-13 11:27:48 -0300

    AtualizaçÃo de requisitos

--- rastreamentoapp/rastreamento_backend/urls.py
+++ rastreamentoapp/rastreamento_backend/urls.py
@@ -28,9 +28,9 @@ urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
     path('api/v1/', include('core.urls')),
     
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    #3path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    #path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    #path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 ]
 
 
